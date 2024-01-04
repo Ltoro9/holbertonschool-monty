4db02d0d75ea78de530b4354bc1b5d980210fd59
Louis Toro <louistororosario@gmail.com>
2024-01-04 13:20:36 -0400

    pop function added

--- main.c
+++ main.c
@@ -2,28 +2,31 @@
 
 int main(int argc, char *argv[])
 {
+	FILE *file;
+	char *line = NULL;
+	size_t len = 0;
+	unsigned int line_number = 0;
+	stack_t *stack = NULL; /*Initialize the stack*/
+	char *opcode = NULL; /*Tokenize the line to get the instruction (opcode)*/
+	int i;
+
 	if (argc != 2) /*Check if the correct number of arguments is provided*/
 	{
 		fprintf(stderr, "Usage: %s file\n", argv[0]);
 		return EXIT_FAILURE;
 	}
 
-	FILE *file = fopen(argv[1], "r"); /*Open the file specified in the argument*/
+	file = fopen(argv[1], "r"); /*Open the file specified in the argument*/
 	if (file == NULL)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		return EXIT_FAILURE;
 	}
 
-	char *line = NULL;
-	size_t len = 0;
-	unsigned int line_number = 0;
-	stack_t *stack = NULL; /*Initialize the stack*/
-
 	while ((getline(&line, &len, file)) != -1) /*Read lines from the file until the end*/
 	{
 		line_number++;
-		char *opcode = strtok(line, " \n"); /*Tokenize the line to get the instruction (opcode)*/
+		opcode = strtok(line, " \n"); /*Tokenize the line to get the instruction (opcode)*/
 		if (opcode == NULL || *opcode == '#')
 		{
 			continue; /*Ignore empty lines*/
@@ -32,10 +35,11 @@ int main(int argc, char *argv[])
 		instruction_t my_instructions[] = {
 			{"push", push},
 			{"pall", pall},
+			{"pop", pop},
 			{NULL, NULL}
 		};
-
-		int i = 0;
+		
+		i = 0;
 		while (my_instructions[i].opcode != NULL) /*Search for the instruction in the array*/
 		{
 			if (strcmp(opcode, my_instructions[i].opcode) == 0)
